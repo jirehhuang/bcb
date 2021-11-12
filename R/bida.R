1e0236566ea569dc513f68e635d196caa9790a92
jirehhuang <jirehhuang@ucla.edu>
2021-11-12 11:45:43 -0800

    Moved several bida-related functions to bida.R, and moved estimation-related functions to estimate.R

--- R/bida.R
+++ R/bida.R
@@ -1,3 +1,482 @@
+######################################################################
+## Functions for computing BIDA posteriors and ARPs
+######################################################################
+
+
+
+# Cache scores for use in bida
+# Modification and extension of calc_parent_score_to_file() in bida
+# Instead of writeLines(), compile all scores and write.table()
+# Supported scores are fml and bge0 (renamed from bge) scores from bida,
+# and bnlearn:::available.scores
+
+cache_scores <- function(data,
+                         settings,
+                         interventions = rep("", nrow(data)),
+                         debug = FALSE){
+
+  ## load relevant settings
+  list2env(settings[c("score", "max_parents", "blmat")],
+           envir = environment())
+  cache_file <- file.path(settings$temp_dir, settings$id)
+
+  ## TODO: check and support blmat
+  blmat <- NULL
+
+  n <- nrow(data)
+  p <- d <- ncol(data)
+  seq_p <- nodes <- seq_len(p)
+  if (score %in% c("fml", "bge0")){
+
+    if (max(abs(apply(data, 2, mean))) > 1e-6){
+      debug_cli_sprintf(debug, "",
+                        "Assumes zero-centered data")
+      data <- apply(data, 2, function(x) x - mean(x))
+    }
+    S <- t(data) %*% data
+  }
+
+  nps <- sum(sapply(0:max_parents, function(x) choose(d-1,x)))
+  debug_cli_sprintf(nps > 1e6,
+                    "abort", "The number of parent sets is > 1e6")
+
+  lns <- c(character(0), toString(d))
+
+  ## fml score from bida
+  if (score == "fml"){
+    n0 <- 1
+    a <- d-1
+    const1 <- -log(pi)*(n-n0)*0.5
+    for (node in nodes){
+      lines <- list()
+      for (k in 0:max_parents){
+        dfam <- 1+k
+        par_sets <- combn(nodes[-node],k)
+        const2 <- lgamma(0.5*(a+n-d+dfam))-
+          lgamma(0.5*(a+n0-d+dfam))+
+          log(n0/n)*(a+n0-d+2*dfam-1)*0.5
+        const <- const1+const2
+        for (i in 1:ncol(par_sets)){
+          par <- par_sets[,i]
+          fam <- c(node,par)
+          if (!is.null(blmat) && any(blmat[par, node] == 1)) next
+          scr <- const-(log(det(S[fam,fam, drop = FALSE]))-log(det(S[par,par, drop = FALSE])))*(n-n0)*0.5
+          lines[[length(lines)+1]] <- paste(trimws(format(round(scr, 6), nsmall=6)),
+                                            k, paste(par,collapse = " "), sep = " ")
+        }
+      }
+      lns <- c(lns, sprintf("%g %g", node, length(lines)), unlist(lines))
+    }
+    ## bge score from bida, renamed to bge0
+  } else if (score == "bge0"){
+    const1 <- -log(pi)*n*0.5
+    for (node in nodes){
+      for (k in 0:max_parents){
+        dfam <- 1+k
+        par_sets <- combn(nodes[-node],k)
+        const2 <- lgamma((dfam+n)*0.5)-lgamma(dfam*0.5)
+        const <- const1+const2
+        for (i in 1:ncol(par_sets)){
+          par <- par_sets[,i]
+          fam <- c(node,par)
+          scr <- const-log(det(S[fam,fam, drop = FALSE]+diag(1,dfam)))*(dfam+n)*0.5+log(det(S[par,par, drop = FALSE]+diag(1,k)))*(k+n)*0.5
+          lines[[length(lines)+1]] <- paste(trimws(format(round(scr, 6), nsmall=6)),
+                                            k, paste(par,collapse = " "), sep = " ")
+        }
+      }
+      lns <- c(lns, sprintf("%g %g", node, length(lines)), unlist(lines))
+    }
+    ## use bnlearn scores, which includes bge
+  } else if (score %in% bnlearn:::available.scores){
+
+    if (score %in% bnlearn:::available.discrete.scores){
+
+      browser()
+
+      ## TODO: check discrete
+    }
+
+    data <- as.data.frame(data)
+    network <- bnlearn::empty.graph(nodes = colnames(data))
+    amat <- bnlearn::amat(network)
+
+    ## TODO: initialize vector instead of appending lines
+
+    for (i in seq_p){
+
+      lines <- list()
+
+      for (k in seq(0, max_parents)){
+
+        par_sets <- combn(seq_p[-i], k)
+
+        ## remove parent sets that violate blmat
+        if (!is.null(blmat))
+          par_sets <- par_sets[, !apply(par_sets, 2,
+                                        function(par) any(blmat[par, i]))]
+
+        len <- length(lines)
+        lines <- c(lines, vector(mode = "list", ncol(par_sets)))
+
+        for (j in seq_len(ncol(par_sets))){
+
+          ## direct parents -> i in network
+          par <- par_sets[, j]
+          amat[par, i] <- 1
+          bnlearn::amat(network) <- amat
+
+          extra.args <- list()
+          extra.args = bnlearn:::check.score.args(score = score, network = network,
+                                                  data = data, extra.args = extra.args)
+
+          ## compute and store score
+          scr <- local_score(network = network, data = data, score = score,
+                             targets = settings$nodes[i], extra.args = extra.args,
+                             interventions = interventions, debug = debug > 1)
+
+          ## TODO: can I increase nsmall
+
+          lines[[len + j]] <- sprintf("%s %g %s",
+                                      trimws(format(round(scr, 6), nsmall=6)),
+                                      k, paste(par, collapse = " "))
+
+          ## disconnect parents -> i in network
+          amat[par, i] <- 0
+        }
+      }
+      lns <- c(lns, sprintf("%g %g", i, length(lines)), unlist(lines))
+    }
+  } else{
+
+    debug_cli_sprintf(TRUE, "abort",
+                      "Unknown score type `%s`; please use bnlearn:::available.scores, fml, or bge0",
+                      score)
+  }
+  ## write table
+  write.table(x = data.frame(lns),
+              file = sprintf("%s_score", cache_file),
+              row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+  debug_cli_sprintf(debug, "success",
+                    "Computed %g scores and saved to %s_score",
+                    length(lns) - d - 1, tail(strsplit(cache_file, "/")[[1]], 1))
+}
+
+
+
+# Compute parent set supports
+# Modification of calc_bida_post()
+
+compute_ps <- function(data,
+                       settings,
+                       interventions = rep("", nrow(data)),
+                       threshold = 0.999,
+                       debug = FALSE){
+
+  ## load relevant settings
+  list2env(settings[c("score", "max_parents", "blmat",
+                      "aps_dir", "nnodes")],
+           envir = environment())
+  cache_file <- file.path(settings$temp_dir, settings$id)
+
+  debug_cli_sprintf(nnodes > 20,
+                    "abort", "Don't use on systems with more than 20 variables")
+
+  ## calculate parent scores and save as temporary files
+  if (!file.exists(sprintf("%s_score", cache_file))){
+
+    debug_cli_sprintf(missing(data),
+                      "abort", "File score missing and argument data missing")
+
+    cache_scores(data = data, settings = settings,
+                 interventions = interventions, debug = debug)
+  }
+
+  ## calculate parent support using the APS solver
+  aps_type <- "modular"
+  system(sprintf("%s/aps %s %s_score %s_support", aps_dir,
+                 aps_type, shQuote(cache_file), shQuote(cache_file)))
+
+  debug_cli_sprintf(!file.exists(sprintf("%s_support", cache_file)),
+                    "abort", "File support missing, and score %s",
+                    ifelse(file.exists(sprintf("%s_score", cache_file)),
+                           "exists", "also missing"))
+
+  ## read in calculated parent support from file
+  ps <- read_aps_ps(settings = settings)
+
+  ## delete support, saving score for arp
+  file.remove(sprintf("%s_support", cache_file))  # keep score for arp
+
+  ## threshold low probability parent sets
+  ps <- threshold_ps(ps = ps, threshold = threshold, debug = debug)
+
+  return(ps)
+}
+
+
+
+# Compute ancestor relation probabilities
+# Modification of calc_arp
+
+compute_arp <- function(data,
+                        settings,
+                        interventions = rep("", nrow(data)),
+                        debug = FALSE){
+
+  ## load relevant settings
+  list2env(settings[c("score", "max_parents", "blmat",
+                      "aps_dir", "nnodes")],
+           envir = environment())
+  cache_file <- file.path(settings$temp_dir, settings$id)
+
+  debug_cli_sprintf(nnodes > 20,
+                    "abort", "Don't use on systems with more than 20 variables")
+
+  ## calculate parent scores and save as temporary files
+  if (!file.exists(sprintf("%s_score", cache_file))){
+
+    debug_cli_sprintf(missing(data),
+                      "abort", "File score missing and argument data missing")
+
+    cache_scores(data = data, settings = settings,
+                 interventions = interventions, debug = debug)
+  }
+
+  ## calculate parent support using the APS solver
+  aps_type <- "ar_modular"
+  system(sprintf("%s/aps %s %s_score %s_arp", aps_dir,
+                 aps_type, shQuote(cache_file), shQuote(cache_file)))
+
+  debug_cli_sprintf(!file.exists(sprintf("%s_arp", cache_file)),
+                    "abort", "File arp missing, and score %s",
+                    ifelse(file.exists(sprintf("%s_score", cache_file)),
+                           "exists", "also missing"))
+
+  ## read in calculated ancestor support from file
+  arp <- as.matrix(read.delim(sprintf("%s_arp", cache_file),
+                              header = FALSE, sep = " ",
+                              skip = settings$nnodes + 1))
+  rownames(arp) <- colnames(arp) <- settings$nodes
+  arp <- t(exp(arp - arp[1, 1]))
+
+  # Delete temporary files
+  file.remove(sprintf("%s_arp", cache_file))
+
+  return(arp)
+}
+
+
+
+######################################################################
+## General relevant functions
+######################################################################
+
+
+
+# Compute local score using bnlearn
+
+local_score <- function(network,
+                        data,
+                        score,
+                        targets,
+                        extra.args,
+                        interventions = rep("", nrow(data)),
+                        debug = FALSE){
+
+  scores <- sapply(targets, function(x) 0)
+
+  ## TODO: add to slides
+
+  ## use all data for target(s) that have not been intervened on
+  targets_obs <- setdiff(targets, unique(interventions))
+  if (length(targets_obs)){
+
+    scores[targets_obs] <- bnlearn:::per.node.score(network = network, data = data,
+                                                    score = score, targets = targets_obs,
+                                                    extra.args = extra.args, debug = debug)
+  }
+  if (length(targets_obs) < length(targets)){
+
+    ## for target(s) that have been intervened on, exclude interventional
+    ## data on that target because it mutilates the graph for that target
+    targets_int <- setdiff(targets, targets_obs)
+    for (target in targets_int){
+
+      scores[target] <- bnlearn:::per.node.score(network = network,
+                                                 data = data[interventions != target,,drop=FALSE],
+                                                 score = score, targets = target,
+                                                 extra.args = extra.args, debug = debug)
+    }
+  }
+  return(scores)
+}
+
+
+
+# Modification of read_in_aps_parent_post() from bida
+# that stores the parent configurations and support data frames
+
+read_aps_ps <- function(settings){
+
+  ## load relevant settings
+  max_parents <- settings$max_parents
+  cache_file <- file.path(settings$temp_dir, settings$id)
+
+  ## read file containing support
+  temp <- read.table(sprintf("%s_support", cache_file),
+                     header = FALSE, sep = " ",
+                     col.names = sprintf("V%g", seq_len(max_parents + 2)),
+                     fill = TRUE)
+
+  p <- temp[1, 1]  # number of variables
+  pos <- 2  # position
+  ps <- vector("list", length = p)
+  nms <- c(sprintf("V%g", seq_len(max_parents)),
+           "score", "support", "ordering")
+
+  ## for each node
+  for (j in seq_len(p)){
+
+    i <- temp[pos, 1]  # node index
+    n_parents <- temp[pos, 2]  # number of parent configurations
+    pos <- pos + 1
+
+    ps[[i]] <- as.data.frame(sapply(nms, function(x) numeric(n_parents),
+                                    simplify = FALSE))
+
+    ## parent configurations of node
+    ps[[i]][seq_len(n_parents),
+            seq_len(max_parents)] <- temp[seq(pos, pos + n_parents - 1),
+                                          seq_len(max_parents) + 2]
+
+    ## convert corresponding scores to support
+    ps[[i]][["score"]] <- temp[seq(pos, pos + n_parents - 1), 1]
+    scr <- exp(ps[[i]][["score"]] -
+                 max(ps[[i]][["score"]]))
+    ps[[i]][["support"]] <- scr / sum(scr)
+
+    ## ordering and order
+    ps[[i]][["ordering"]] <- order(ps[[i]][["support"]],
+                                   decreasing = TRUE)
+    # ps[[i]][["order"]][ps[[i]][["ordering"]]] <- seq_len(n_parents)
+
+    ## iterate position
+    pos <- pos + n_parents
+  }
+  names(ps) <- settings$nodes
+
+  return(ps)
+}
+
+
+
+# Function to threshold parent configurations with low support
+# Modification of calc_bida_post()
+
+threshold_ps <- function(ps,
+                         threshold = 0.999,
+                         debug = FALSE){
+
+  ## remove lowest probability parent sets
+  ## beyond some cumulative support threshold
+
+  if (threshold >= 1){
+
+    debug_cli_sprintf(debug, "info",
+                      "No parent support thresholding requested")
+    return(ps)
+  }
+
+  for (i in seq_len(length(ps))){
+
+    ## first location where cumsum is at least threshold
+    pos <- match(TRUE,
+                 cumsum(ps[[i]]$support[ps[[i]]$ordering]) >= threshold)
+    if (is.na(pos))
+      pos <- nrow(ps[[i]])
+
+    ## remove parents with low support
+    ps[[i]]$support[-ps[[i]]$ordering[seq_len(pos)]] <- 0
+
+    debug_cli_sprintf(debug > 1, "", "Thresholded %g out of %g parent sets for node %s",
+                      nrow(ps[[i]]) - pos, nrow(ps[[i]]), names(ps)[i])
+
+    ## normalize probabilities
+    ps[[i]]$support <- ps[[i]]$support / sum(ps[[i]]$support)
+  }
+  return(ps)
+}
+
+
+
+## Convert parent support to edge support matrix
+
+ps2es <- function(ps,
+                  settings){
+
+  nnodes <- settings$nnodes
+  max_parents <- settings$max_parents
+  # parents <- setdiff(names(ps[[1]]), c("score", "support", "ordering"))
+  parents <- seq_len(max_parents)
+
+  es <- bnlearn::amat(bnlearn::empty.graph(settings$nodes))
+
+  for (i in seq_len(nnodes - 1)){
+
+    for (j in seq(i + 1, nnodes)){
+
+      # j -> i
+      es[j, i] <- sum(ps[[i]]$support
+                      [apply(ps[[i]][parents],
+                             1, function(x) j %in% x)])
+
+      # i -> j
+      es[i, j] <- sum(ps[[j]]$support
+                      [apply(ps[[j]][parents],
+                             1, function(x) i %in% x)])
+    }
+  }
+  return(es)
+}
+
+
+
+######################################################################
+## Compile and test
+######################################################################
+
+
+
+# Check operating system; Windows not yet supported
+
+check_os <- function(){
+
+  debug_cli_sprintf(.Platform$OS == "windows", "abort",
+                    "bcb is not currently supported on Windows")
+}
+
+
+
+# Get bida aps executable or directory
+
+get_bida <- function(dir = FALSE){
+
+  package_dir <- find.package("bcb", lib.loc = .libPaths())
+  aps_dir <- file.path(package_dir, "bida", "aps-0.9.1", "aps")
+
+  if (dir){
+
+    return(aps_dir)
+  }
+  else{
+
+    return(file.path(aps_dir, "aps"))
+  }
+}
+
+
+
 # Compile bida aps using make
 
 compile_bida <- function(aps_dir = get_bida(dir = TRUE),
@@ -103,33 +582,4 @@ test_bida <- function(debug = FALSE){
 
   debug_cli_sprintf(debug, "success",
                     "Successfully computed bida ancestor probabilities")
-}
-
-
-
-# Get bida aps executable or directory
-
-get_bida <- function(dir = FALSE){
-
-  package_dir <- find.package("bcb", lib.loc = .libPaths())
-  aps_dir <- file.path(package_dir, "bida", "aps-0.9.1", "aps")
-
-  if (dir){
-
-    return(aps_dir)
-  }
-  else{
-
-    return(file.path(aps_dir, "aps"))
-  }
-}
-
-
-
-# Check operating system; Windows not yet supported
-
-check_os <- function(){
-
-  debug_cli_sprintf(.Platform$OS == "windows", "abort",
-                    "bcb is not currently supported on Windows")
 }
---
+++ R/estimate.R
@@ -0,0 +1,196 @@
+######################################################################
+## Functions for estimating effects and standard errors
+######################################################################
+
+
+
+# Compute back-door adjustment estimates
+
+compute_bda <- function(data,
+                        settings,
+                        rounds,
+                        target = settings$target,
+                        nig_bda = NULL,
+                        a_0 = 1,
+                        b_0 = 1,
+                        m_0 = 0,
+                        Lambda_0 = 1,
+                        debug = FALSE){
+
+  t <- nrow(data)
+  p <- settings$nnodes
+  seq_p <- seq_len(p)
+  nodes <- settings$nodes
+  parents <- seq_len(settings$max_parents)
+  m_00 <- m_0
+  Lambda_00 <- Lambda_0
+
+  ## use normal-inverse-gamma (nig) linear model if
+  ## observational data n < max_parents + 2
+  if (is.null(nig_bda)){
+
+    nig_bda <- any(sapply(seq_p, function(i){
+
+      sum(get_bool_data(t = t, i = i, settings = settings, rounds = rounds))
+
+    }) < (settings$max_parents + 2))
+  }
+  debug_cli_sprintf(debug, "info",
+                    "Computing back-door effects with %s linear model",
+                    ifelse(nig_bda, "Bayesian Normal-inverse-gamma", "standard"))
+
+  ## initialize storage structure
+  if (is.null(rounds[["bda"]])){
+
+    bda <- lapply(seq_p, function(i){
+
+      lapply(seq_p, function(j) if (j != i){
+
+        # for bda estimate and joint estimate:
+        # last t where updated, estimate, and standard error
+        data.frame(t_bda = rep(NA, nrow(rounds$ps[[i]])),
+                   est_bda = NA, se_bda = NA,
+                   t_joint = NA, est_joint = NA, se_joint = NA)
+
+      } else NULL)
+    })
+    names(bda) <- nodes
+
+  } else{
+
+    bda <- rounds$bda
+  }
+  if (settings$type == "bn.fit.gnet"){
+
+    ## Gaussian implementation
+    for (i in seq_p){
+
+      bool_data <- get_bool_data(t = t, i = i,
+                                 settings = settings, rounds = rounds)
+
+      pars <- as.matrix(rounds$ps[[i]][parents])
+      temp <- bda[[i]]
+      n <- sum(bool_data)
+
+      Xy <- as.matrix(data[bool_data, , drop=FALSE])
+      if (max(abs(apply(Xy, 2, mean))) > 1e-6){
+        debug_cli_sprintf(debug > 1, "",
+                          "Assumes zero-centered data")
+        Xy <- apply(Xy, 2, function(x) x - mean(x))
+      }
+      XytXy <- t(Xy) %*% Xy
+
+      for (l in rounds$ps[[i]]$ordering){
+
+        if (rounds$ps[[i]]$support[l] == 0) break
+
+        k <- pars[l, !is.na(pars[l, ])]  # indices of parents
+        n_parents <- length(k)  # number of parents
+
+        if (nig_bda){
+
+          # prior hyperparameters
+          m_0 <- matrix(m_00, n_parents + 1, 1)  # prior mean
+          Lambda_0 <- diag(n_parents + 1) / Lambda_00  # prior variance
+
+          # update hyperparameters
+          Lambda_n <- XytXy[c(i, k), c(i, k), drop = FALSE] + Lambda_0
+          a_n <- a_0 + n / 2
+          V_n <- solve(Lambda_n)
+        }
+
+        for (j in if (is.null(target)) seq_p[-i] else target){
+
+          if (j %in% k){  # j -> i, so i -/-> j
+
+            temp[[j]][l, seq(1, 6)] <- rep(0, 6)
+
+          } else {
+
+            ## compute bda effect
+            if (is.na(temp[[j]][l, 1]) ||  # not computed bda effect
+                any(bool_data[seq(temp[[j]][i, 1] + 1, t)])){  # added obs data
+
+              beta <- 0
+              se <- 0
+
+              if (nig_bda){
+
+                ## normal inverse gamma linear model
+                lm_nig(Xty = XytXy[c(i, k), j], m_0 = m_0, Lambda_0 = Lambda_0,
+                       Lambda_n = Lambda_n, V_n = V_n, yty = XytXy[j, j],
+                       a_n = a_n, b_0 = b_0, beta = beta, se = se)
+              } else{
+
+                ## standard linear model
+                lm_cpp(X = Xy[, c(i, k), drop = FALSE], y = Xy[, j],
+                       beta = beta, se = se)
+              }
+              temp[[j]][i, seq_len(3)] <- c(t, beta, se)
+            }
+
+            ## compute joint estimate
+            if (t <= settings$n_obs){
+
+              temp[[j]][l, seq(4, 6)] <- temp[[j]][l, seq(1, 3)]
+
+            } else if (temp[[j]][l, 1] == t ||  # just updated bda effect
+                       rounds$selected$interventions[t] ==
+                       nodes[i]){  # most recent intervention is on i
+
+              browser()
+
+              ## TODO: joint estimate
+            }
+          }
+        }
+      }
+      bda[[i]] <- temp
+    }
+  } else if (settings$type == "bn.fit.dnet"){
+
+    browser()
+
+    ## TODO: discrete implementation
+
+  }
+  return(bda)
+}
+
+
+
+######################################################################
+## General relevant functions
+######################################################################
+
+
+
+# Indicate data rows that can be used for bda
+
+get_bool_data <- function(t,
+                          i,
+                          settings,
+                          rounds,
+                          debug = FALSE){
+
+  bool_data <- rep(TRUE, t)
+
+  if (t > settings$n_obs){
+
+    ## add interventional data with node j such that either
+    ## Pr(i -> j) or Pr(j -> target) is low; i.e. j does not
+    ## block a directed path from i to target
+    bool_arms <- sapply(rounds$arms, function(arm){
+
+      max(rounds$arp[i, arm$node],
+          rounds$arp[arm$node, settings$target]) < settings$eta
+    })
+    bool_data[rounds$selected$arm[seq_len(t)] > 0] <-
+      bool_arms[rounds$selected$arm[seq_len(t)]]
+
+    debug_cli_sprintf(debug && sum(bool_data) > settings$n_obs, "",
+                      "Using %g rows of experimental data for back-door adjustment",
+                      sum(bool_data) - settings$n_obs)
+  }
+  return(bool_data)
+}
