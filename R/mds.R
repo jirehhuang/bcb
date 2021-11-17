ca90485eb5eccb78fd24a6990b3703a66cf8ee68
jirehhuang <jirehhuang@ucla.edu>
2021-11-17 08:50:24 -0800

    Added ps2gobnilp(), sampler2amat(), and execute_mds() for sampling a DAG based on ps

--- R/mds.R
+++ R/mds.R
@@ -1,4 +1,97 @@
 ######################################################################
+## Functions for executing mds
+######################################################################
+
+
+
+execute_mds <- function(ps,
+                        settings,
+                        seed = 1,
+                        debug = FALSE){
+
+  cache_file <- file.path(settings$temp_dir, settings$id)
+  gob <- ps2gobnilp(ps = ps, settings = settings, debug = debug)
+
+  ## write table
+  write.table(x = data.frame(gob),
+              file = sprintf("%s_gobnilp", cache_file),
+              row.names = FALSE, col.names = FALSE, quote = FALSE)
+
+  start_time <- Sys.time()
+  sampler <- sys::exec_internal(cmd = get_mds(),
+                                args = c("nonsymmetric",
+                                         sprintf("%s_gobnilp", cache_file),
+                                         seed))
+  end_time <- Sys.time()
+  sampler_time <- as.numeric(end_time - start_time, units = "secs")
+
+  # browser()  # TODO: temporary for debugging
+
+  ## convert to amat
+  text <- sys::as_text(sampler$stdout)
+  graph <- sampler2amat(text = text[length(text)])
+  rownames(graph) <- colnames(graph) <- settings$nodes
+
+  return(graph)
+}
+
+
+
+######################################################################
+## General relevant functions
+######################################################################
+
+
+
+ps2gobnilp <- function(ps,
+                       settings,
+                       debug = FALSE){
+
+  p <- length(ps)
+  seq_p <- seq_len(p)
+  max_parents <- settings$max_parents
+
+  lns <- lapply(seq_p, function(i){
+
+    x <- ps[[i]]
+    x <- cbind(n_parents = apply(x, 1, function(y){
+
+      sum(!is.na(y[seq_len(max_parents)]))
+    }), x)
+
+    c(sprintf("%g %g", i, nrow(x)),
+      apply(x, 1, function(y){
+
+        paste(y[c(max_parents + 2, 1, seq_len(y[1]) + 1)],
+              collapse = " ")
+      }))
+  })
+  lns <- c(p, do.call(c, lns))
+
+  return(lns)
+}
+
+
+
+sampler2amat <- function(text){
+
+  texts <- strsplit(text, "},")[[1]]
+
+  node_list <- stringr::str_extract_all(texts, "[0-9]+")
+  nodes <- sapply(node_list, `[`, 1)
+
+  amat <- bnlearn::amat(bnlearn::empty.graph(nodes))
+
+  for (node in node_list){
+
+    amat[node[-1], node[1]] <- 1
+  }
+  return(amat)
+}
+
+
+
+######################################################################
 ## Compile and test
 ######################################################################
 
