b1a83156c5afcfc82f036f64a7e07cfda2a4b5c4
jirehhuang <jirehhuang@ucla.edu>
2021-11-18 20:08:46 -0800

    Added int2targets() for converting interventions to targets and target.index as in pcalg; updated lookup_score class to take interventions for gies(); updated bnlearn_score class to take interventions instead of targets and target.index

--- R/gies.R
+++ R/gies.R
@@ -1,13 +1,20 @@
+######################################################################
+## Functions for efficiently executing gies
+######################################################################
+
+
+
 # Estimate adjacency matrix using gies
 
-estimate_gies <- function(cache,
+estimate_gies <- function(ps,
                           settings,
+                          interventions,
                           dag = TRUE,
                           debug = FALSE){
 
-  nodes <- names(cache)
-
-  score <- new("lookup_score", cache = cache,
+  nodes <- settings$nodes
+  score <- new("lookup_score", ps = ps,
+               interventions = interventions,
                nodes = nodes)
 
   ## TODO: fixedGaps and adaptive with blmat
@@ -23,4 +30,25 @@ estimate_gies <- function(cache,
     amat <- phsl:::pdag2dag_cpp(amat, nodes = nodes)$graph
 
   return(amat)
+}
+
+
+
+# Convert interventions to targets and target.index as in pcalg
+
+int2targets <- function(interventions,
+                        nodes){
+
+  uint <- unique(interventions)
+
+  targets <- lapply(uint, function(x){
+    int <- match(x, nodes)
+    if (is.na(int))
+      int <- integer(0)
+    return(int)
+  })
+  target.index <- match(interventions, uint)
+
+  return(list(targets = targets,
+              target.index = target.index))
 }
