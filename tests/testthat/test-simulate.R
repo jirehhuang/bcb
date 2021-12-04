7f84f1ef210bf4374d3f38efbb225f7f7f99eae8
jirehhuang <jirehhuang@ucla.edu>
2021-12-04 15:03:56 -0800

    Updated simulate_method() test

--- tests/testthat/test-simulate.R
+++ tests/testthat/test-simulate.R
@@ -2,15 +2,21 @@ debug <- 3
 
 
 
+## simulate_method()
 settings <- list(method = "random",
                  n_obs = 10, n_int = 10)
-path <- file.path(gsub("/tests.*", "", getwd()),
-                  "tests", "temp", "asia")
 
-result <- simulate_method(method_num = 1,
-                          settings = settings,
-                          path = path,
-                          n_cores = 1,
-                          seed0 = 0,
-                          resimulate = FALSE,
-                          debug = 3)
+networks <- c("asia")
+
+for (network in networks){
+
+  path <- file.path(gsub("/tests.*", "", getwd()),
+                    "tests", "temp", sprintf("test-%s", network))
+
+  simulate_method(method_num = 1,
+                  settings = settings,
+                  path = path,
+                  n_cores = 4,
+                  resimulate = TRUE,
+                  debug = 3)
+}
