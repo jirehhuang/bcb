82fcc02f4362c5f4d380b8637c7cbd09cdc798ae
jirehhuang <jirehhuang@ucla.edu>
2021-12-04 14:51:01 -0800

    Added amat2cpdag() for when cycles exist in estimated (p)dag

--- R/graph.R
+++ R/graph.R
@@ -17,13 +17,10 @@ eval_graph <- function(est, true, cp = FALSE){
   ## convert to cpdags
   if (cp){
 
-    bn <- bnlearn::empty.graph(nodes = colnames(true))
-
-    bnlearn::amat(bn) <- true
-    true <- bnlearn::amat(bnlearn::cpdag(bn))
-
-    bnlearn::amat(bn) <- est
-    est <- bnlearn::amat(bnlearn::cpdag(bn))
+    true <- amat2cpdag(amat = true,
+                       nodes = colnames(true), debug = 0)
+    est <- amat2cpdag(amat = est,
+                      nodes = colnames(est), debug = 0)
   }
 
   est_undir <- est * t(est)
@@ -60,4 +57,67 @@ eval_graph <- function(est, true, cp = FALSE){
     df$JI <- 1
 
   return(df)
+}
+
+
+
+# Convert adjacency matrix to CPDAG
+# Circumvent converting amat to a bn, which fails
+# if amat contains cycles
+
+amat2cpdag <- function(amat,
+                       nodes = colnames(amat),
+                       debug = 0){
+
+  if (sum(amat) == 0){
+
+    return(amat)
+  }
+  mode(amat) <- "integer"
+  is_acyclic <-
+    bnlearn:::is.acyclic(arcs = bnlearn:::amat2arcs(a = amat, nodes = nodes),
+                         nodes = nodes, directed = TRUE, debug = debug >= 3)
+  ## phsl version, for remove_invalid
+  if (!is_acyclic){
+
+    ## get v-structures
+    vsmat <- amat * 0
+    vs <- phsl:::vstructs_cpp(amat = amat, vsmat = vsmat,
+                              debug = debug >= 3)
+
+    ## apply v-structures
+    pdag <- (amat | t(amat)) -  # undirected graph
+      (vsmat | t(vsmat)) +  # remove v-structures
+      vsmat  # orient v-structures
+
+    ## apply cpdag rules
+    ## remove_invalid: remove arc if conflicting R2
+    mode(pdag) <- "integer"
+    cpdag <- phsl:::apply_cpdag_rules(pdag = pdag, nodes = nodes,
+                                      remove_invalid = TRUE,
+                                      debug = debug >= 3)
+    rownames(cpdag) <-
+      colnames(cpdag) <- nodes
+
+    ## TODO: remove; temporary for debugging
+    is_acyclic <-
+      bnlearn:::is.acyclic(arcs = bnlearn:::amat2arcs(a = cpdag, nodes = nodes),
+                           nodes = nodes, directed = TRUE, debug = debug >= 3)
+    if (!is_acyclic){
+
+      debug_cli(TRUE, cli::cli_alert_danger,
+                "CPDAG still contains cycles")
+      browser()
+    }
+  } else{
+
+    ## bnlearn version
+    mode(amat) <- "integer"
+    arcs <- bnlearn:::amat2arcs(a = amat, nodes = nodes)
+    cpdag = .Call(bnlearn:::call_cpdag, arcs = arcs, nodes = nodes,
+                  moral = FALSE, fix = FALSE, wlbl = FALSE,
+                  whitelist = NULL, blacklist = NULL,
+                  illegal = NULL, debug = debug >= 3)
+  }
+  return(cpdag)
 }
